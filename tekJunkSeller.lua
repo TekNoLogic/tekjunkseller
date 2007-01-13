547929d98bf47fe7131ee345a086d691234108aa
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-13 19:20:54 +0000

    Importing tekJunkSeller
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekJunkSeller@169 86fe6d9a-1522-0410-a387-bf9db416f0a0

---
+++ tekJunkSeller.lua
@@ -0,0 +1,23 @@
+
+
+local function SellItem(bag, slot)
+	if not bag or not slot then return end
+	ShowMerchantSellCursor(1)
+	UseContainerItem(bag, slot)
+end
+
+local function OnEvent()
+	for bag=0,4 do
+		for slot=0,GetContainerNumSlots(bag) do
+			if select(3, GetItemInfo(GetContainerItemLink(bag, slot))) == 0 then SellItem(bag, slot) end
+		end
+	end
+end
+
+
+local f = CreateFrame()
+f:RegisterEvent("MERCHANT_SHOW")
+f:SetScript("OnEvent", OnEvent)
+
+
+
